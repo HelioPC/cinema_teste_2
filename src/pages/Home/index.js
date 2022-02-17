ad0f59e4a4791c97cf1e773f1a2fb0d1c3519947
Eliúde Vemba <sweeteliude@gmail.com>
2022-02-17 07:34:56 +0100

    Setup to start"

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import 'home.css';
+
+export default () => {
+    return (
+        <div className="home">
+            Olá Home
+        </div>
+      );
+}
---
+++ src/pages/Other/index.js
@@ -0,0 +1,9 @@
+import 'home.css';
+
+export default () => {
+    return (
+        <div className="other">
+            Olá Mundo
+        </div>
+      );
+}
