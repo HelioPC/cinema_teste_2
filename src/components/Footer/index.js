48db417d5cc1d69b59fd19fec933f394682d5697
Eliúde Vemba <sweeteliude@gmail.com>
2023-03-15 23:17:52 +0100

    Test 01/04
    Switch to nextjs

--- src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import './footer.css';
-
-export default function Footer() {
-    return (
-        <footer>
-            Este é o footer <span role='img' aria-label='heart'>👑</span>
-        </footer>
-    )
-}
